5909f9919cf076aff993ddf26e560487279f58cb
yangyang <1505399301@qq.com>
2021-12-05 20:14:45 +0800

    两部分代码

---
+++ IO/1_server.cpp
@@ -0,0 +1,118 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+
+
+int main( int argc, char* argv[] )
+{
+    if( argc <= 3 )
+    {
+        printf( "arg: ip port file\n" );
+        return 1;
+    }
+    const char* ip = argv[1];
+    int port = atoi( argv[2] );
+
+    int filefd = open( argv[3], O_CREAT | O_WRONLY | O_TRUNC, 0666 );
+    assert( filefd > 0 );
+
+    printf( "* runing %s : %s:%d file:%s \n", basename( argv[0] ), ip, port, argv[3] );
+    printf( "* print log write to file:%s \n", argv[3] );
+
+    close( STDOUT_FILENO );
+    dup( filefd );
+
+    struct sockaddr_in address;
+    bzero( &address, sizeof( address ) );
+    address.sin_family = AF_INET;
+    inet_pton( AF_INET, ip, &address.sin_addr );
+    address.sin_port = htons( port );
+
+    int sock = socket( PF_INET, SOCK_STREAM, 0 );
+    assert( sock >= 0 );
+
+    int ret = bind( sock, ( struct sockaddr* )&address, sizeof( address ) );
+    assert( ret != -1 );
+
+    ret = listen( sock, 5 );
+    assert( ret != -1 );
+
+    struct sockaddr_in client;
+    socklen_t clen = sizeof( client );
+    int connfd = accept( sock, ( struct sockaddr* )&client, &clen );
+    if ( connfd < 0 )
+    {
+        printf( "errno is: %d\n", errno );
+    }
+    else
+    {
+        // 客户端链接信息
+        char remote[INET_ADDRSTRLEN];
+        printf( "client connected : %s:%d\n", 
+                    inet_ntop( AF_INET, &client.sin_addr, remote, INET_ADDRSTRLEN ), ntohs( client.sin_port ) );
+
+        int sfd[2];
+        int ret = pipe( sfd );
+        assert( ret != -1 );
+
+        int ffd[2];
+        ret = pipe( ffd );
+        assert( ret != -1 );
+
+        const int sz = 32768;
+
+        while(true)
+        {
+            
+            usleep(100000);
+
+            ret = splice( connfd, nullptr, sfd[1], nullptr, sz, SPLICE_F_MORE | SPLICE_F_MOVE );
+            if(ret == -1){
+                perror("splice1");
+            }
+            
+            write( connfd, "recvmsg\n", 8 );
+
+            // 不可以直接转
+            // ret = tee( sfd[0], connfd, sz, SPLICE_F_NONBLOCK );
+            // if(ret == -1){
+            //     perror("tee");
+            // }
+
+            // 只能管道间转
+            ret = tee( sfd[0], ffd[1], sz, SPLICE_F_NONBLOCK );
+            if(ret == -1){
+                perror("tee");
+            }
+
+            ret = splice( sfd[0], nullptr, connfd, nullptr, sz, SPLICE_F_MORE | SPLICE_F_MOVE );
+            if(ret == -1){
+                perror("splice2");
+            }
+
+            ret = splice( ffd[0], nullptr, filefd, nullptr, sz, SPLICE_F_MORE | SPLICE_F_MOVE );
+            if(ret == -1){
+                perror("splice3");
+            }
+
+        }
+
+        close( connfd );
+        close( sfd[0] );
+        close( sfd[1] );
+        close( ffd[0] );
+        close( ffd[1] );
+    }
+    
+
+    close( sock );
+    return 0;
+}
+
---
+++ socket/1_client.cpp
@@ -0,0 +1,108 @@
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <iostream>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main( int argc, char* argv[] )
+{
+    if( argc <= 2 )
+    {
+        printf( "arg: ip port \n" );
+        return 1;
+    }
+
+    const char* ip = argv[1];
+    int port = atoi( argv[2] );
+    
+    printf( "* runing %s : %s:%d \n", basename(argv[0]), ip, port );
+
+    int sock = socket( PF_INET, SOCK_STREAM, 0 );
+    assert( sock >= 0 );
+    
+    // 设置在线接收带外数据
+    int on = 1;
+    setsockopt( sock, SOL_SOCKET, SO_OOBINLINE, &on, sizeof(on) );
+
+    // 设置写缓冲区大小
+    int sbsz = 4096;
+    int slen = sizeof(sbsz);
+    setsockopt( sock, SOL_SOCKET, SO_SNDBUF, &sbsz, sizeof(sbsz) );
+    getsockopt( sock, SOL_SOCKET, SO_SNDBUF, &sbsz, (socklen_t*)&slen );
+    printf( "* the send buffer size after setting is %d\n", sbsz );
+
+    // 设置读缓冲区大小
+    int rbsz = 4096;
+    int rlen = sizeof(rbsz);
+    setsockopt( sock, SOL_SOCKET, SO_RCVBUF, &rbsz, sizeof(rbsz) );
+    getsockopt( sock, SOL_SOCKET, SO_RCVBUF, &rbsz, (socklen_t*)&rlen );
+    printf( "* the receive buffer size after settting is %d\n", rbsz );
+
+    // 设置地址和端口
+    struct sockaddr_in address;
+    bzero( &address, sizeof(address) );
+    address.sin_family = AF_INET;
+    inet_pton( AF_INET, ip, &address.sin_addr );
+    address.sin_port = htons( port );
+
+    // 连接
+    if( connect( sock, (struct sockaddr*)&address, sizeof(address) ) < 0){
+        perror("connect");
+        return 1;
+    }
+    
+    const int bsz = 4096;
+    char buffer[ bsz ];
+    while(1){
+
+        printf("-input msg...\n");
+        std::cin >> buffer;
+        printf("\n");
+
+        int len = strlen( buffer );
+        if(len>3 && buffer[0]=='O' && buffer[1]=='O' && buffer[2]=='B' )
+        {
+            send( sock, buffer, len, MSG_OOB );
+        }
+        else
+        {
+            send( sock, buffer, len, 0);
+        }
+
+        if(strcmp(buffer,"quit")==0)break;
+
+        usleep(100000);
+
+        for (int i = 0; i < 10; ++i)
+        {
+            int ret = sockatmark(sock);
+            if(ret==1){
+                printf("at OOB mark\n");
+            }
+            else if(ret==-1)
+            {
+                perror("sockatmark");
+            }
+
+            memset( buffer, '\0', bsz );
+            len = recv( sock, buffer, bsz, MSG_DONTWAIT );
+            if(len>0){
+                printf( "from server msg: '%s'\n", buffer );
+            }
+            else
+            {
+                break;
+            }
+            usleep(10000);
+        }
+
+    }
+
+    close(sock);
+
+    return 0;
+}
